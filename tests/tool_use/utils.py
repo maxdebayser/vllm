53869d20fc1ddeb37a35c22c7ce48d97b5035a7a
Max de Bayser <mbayser@br.ibm.com>
2024-09-27 16:32:09 -0300

    add support for streaming
    Signed-off-by: Max de Bayser <mbayser@br.ibm.com>

--- tests/tool_use/utils.py
+++ tests/tool_use/utils.py
@@ -87,7 +87,16 @@ CONFIGS: Dict[str, ServerConfig] = {
         "call the tool. Otherwise, answer the user's query directly "
         "without calling a tool. DO NOT CALL A TOOL THAT IS IRRELEVANT "
         "to the user's question - just respond to it normally."
+    },
+    "granite": {
+        "model":
+        "ibm-granite/granite-20b-functioncalling",
+        "arguments": [
+            "--tool-call-parser", "granite", "--chat-template",
+            str(VLLM_PATH / "examples/tool_chat_template_granite.jinja")
+        ],
     }
+
 }
 
 WEATHER_TOOL: ChatCompletionToolParam = {
--- vllm/entrypoints/openai/tool_parsers/granite_tool_parser.py
+++ vllm/entrypoints/openai/tool_parsers/granite_tool_parser.py
@@ -1,15 +1,21 @@
 import json
+from json import JSONDecodeError, JSONDecoder
 import re
-from typing import Sequence, Union
-
-from vllm.entrypoints.openai.protocol import (DeltaMessage,
+from typing import Dict, Sequence, Union
+import partial_json_parser
+from partial_json_parser.core.options import Allow
+from vllm.entrypoints.openai.protocol import (DeltaFunctionCall, DeltaMessage,
+                                              DeltaToolCall,
                                               ExtractedToolCallInformation,
                                               FunctionCall, ToolCall)
 from vllm.entrypoints.openai.tool_parsers.abstract_tool_parser import (
     ToolParser)
 from vllm.logger import init_logger
+from vllm.utils import random_uuid
 from vllm.transformers_utils.tokenizer import AnyTokenizer
+from vllm.entrypoints.openai.tool_parsers.utils import find_common_prefix, is_complete_json, partial_json_loads
 
+import traceback
 logger = init_logger(__name__)
 
 
@@ -30,6 +36,7 @@ class GraniteToolParser(ToolParser):
                                                 content=model_output)
 
         else:
+            dec = JSONDecoder()
             try:
                 matches = list(self.tool_call_regex.finditer(model_output))
                 logger.debug("Found %d tool call matches", len(matches))
@@ -44,11 +51,8 @@ class GraniteToolParser(ToolParser):
                     next_function_call_start = (matches[i + 1].start() if
                                                 i + 1 < len(matches) else None)
 
-                    # extract the full JSON object using bracket matching via
-                    # start_of_json and optional end index
-                    full_json_str = extract_full_json(
-                        model_output, start_of_json, next_function_call_start)
-                    raw_function_calls.append(json.loads(full_json_str))
+                    raw_function_calls.append(
+                        dec.raw_decode(model_output[start_of_json:next_function_call_start])[0])
 
                 logger.debug("Extracted %d tool calls",
                              len(raw_function_calls))
@@ -73,6 +77,7 @@ class GraniteToolParser(ToolParser):
             except Exception as e:
                 logger.error("Error in extracting tool call from response %s",
                              e)
+                traceback.print_exception(e)
                 return ExtractedToolCallInformation(tools_called=False,
                                                     tool_calls=[],
                                                     content=model_output)
@@ -86,23 +91,142 @@ class GraniteToolParser(ToolParser):
         current_token_ids: Sequence[int],
         delta_token_ids: Sequence[int],
     ) -> Union[DeltaMessage, None]:
-        raise NotImplementedError(
-            "streaming tool call parsing has not yet been implemented"
-            "for Granite")
 
+        if len(current_text) < len(self.bot_token) and self.bot_token.startswith(current_text):
+            return None
 
-def extract_full_json(text, start_index, end_index=None):
-    """
-    Extracts the full JSON object from text starting at `start_index` 
-    and optionally up to `end_index`.
-    """
-    brace_count = 0
-    json_start = start_index
-    for i, char in enumerate(text[start_index:end_index], start=start_index):
-        if char == "{":
-            brace_count += 1
-        elif char == "}":
-            brace_count -= 1
-        if brace_count == 0:
-            return text[json_start:i + 1]
-    raise ValueError("Unbalanced braces in the input string")
+        if not current_text.startswith(self.bot_token):
+            return DeltaMessage(content=delta_text)
+
+        # bit mask flags for partial JSON parsing. If the name hasn't been
+        # sent yet, don't allow sending
+        # an incomplete string since OpenAI only ever (as far as I have
+        # seen) allows sending the entire tool/ function name at once.
+        flags = Allow.ALL if self.current_tool_name_sent \
+            else Allow.ALL & ~Allow.STR
+        try:
+            tool_call_arr = []
+            is_complete = []
+            try:
+                start_idx = len(self.bot_token)
+                while start_idx < len(current_text):
+                    (obj,
+                     end_idx) = partial_json_loads(current_text[start_idx:],
+                                                   flags)
+                    is_complete.append(
+                        is_complete_json(current_text[start_idx:start_idx +
+                                                      end_idx]))
+                    start_idx += end_idx + len(self.bot_token)+1
+                    tool_call_arr.append(obj)
+            except partial_json_parser.core.exceptions.MalformedJSON:
+                logger.debug('not enough tokens to parse into JSON yet')
+                return None
+
+            # select as the current tool call the one we're on the state at
+            current_tool_call: Dict = tool_call_arr[self.current_tool_id] \
+                if len(tool_call_arr) > 0 else {}
+
+            # case -- if no tokens have been streamed for the tool, e.g.
+            #   only the array brackets, stream nothing
+            if len(tool_call_arr) == 0:
+                return None
+
+            # case: we are starting a new tool in the array
+            #   -> array has > 0 length AND length has moved past cursor
+            elif (len(tool_call_arr) > 0
+                  and len(tool_call_arr) > self.current_tool_id + 1):
+
+                # if we're moving on to a new call, first make sure we
+                # haven't missed anything in the previous one that was
+                # auto-generated due to JSON completions, but wasn't
+                # streamed to the client yet.
+                if self.current_tool_id >= 0:
+                    cur_arguments = current_tool_call.get("arguments")
+                    if cur_arguments:
+                        cur_args_json = json.dumps(cur_arguments)
+                        sent = len(
+                            self.streamed_args_for_tool[self.current_tool_id])
+                        argument_diff = cur_args_json[sent:]
+
+                        logger.debug("got arguments diff: %s", argument_diff)
+                        delta = DeltaMessage(tool_calls=[
+                            DeltaToolCall(index=self.current_tool_id,
+                                          function=DeltaFunctionCall(
+                                              arguments=argument_diff).
+                                          model_dump(exclude_none=True))
+                        ])
+                        self.streamed_args_for_tool[
+                            self.current_tool_id] += argument_diff
+                    else:
+                        delta = None
+                else:
+                    delta = None
+                # re-set stuff pertaining to progress in the current tool
+                self.current_tool_id = len(tool_call_arr) - 1
+                self.current_tool_name_sent = False
+                self.streamed_args_for_tool.append("")
+                logger.debug("starting on new tool %d", self.current_tool_id)
+                return delta
+
+            # if the current tool name hasn't been sent, send if available
+            # - otherwise send nothing
+            elif not self.current_tool_name_sent:
+                function_name = current_tool_call.get("name")
+                if function_name:
+
+                    delta = DeltaMessage(tool_calls=[
+                        DeltaToolCall(index=self.current_tool_id,
+                                      type="function",
+                                      id=f"chatcmpl-tool-{random_uuid()}",
+                                      function=DeltaFunctionCall(
+                                          name=function_name).model_dump(
+                                              exclude_none=True))
+                    ])
+                    self.current_tool_name_sent = True
+                else:
+                    delta = None
+
+            # now we know we're on the same tool call and we're streaming
+            # arguments
+            else:
+                cur_arguments = current_tool_call.get("arguments")
+                delta = None
+
+                if cur_arguments:
+                    sent = len(
+                        self.streamed_args_for_tool[self.current_tool_id])
+                    cur_args_json = json.dumps(cur_arguments)
+                    prev_arguments = self.prev_tool_call_arr[
+                        self.current_tool_id].get("arguments")
+
+                    argument_diff = None
+                    if is_complete[self.current_tool_id]:
+                        argument_diff = cur_args_json[sent:]
+                    elif prev_arguments:
+                        prev_args_json = json.dumps(prev_arguments)
+                        if cur_args_json != prev_args_json:
+
+                            prefix = find_common_prefix(
+                                prev_args_json, cur_args_json)
+                            argument_diff = prefix[sent:]
+
+                    if argument_diff is not None:
+                        delta = DeltaMessage(tool_calls=[
+                            DeltaToolCall(index=self.current_tool_id,
+                                          function=DeltaFunctionCall(
+                                              arguments=argument_diff).
+                                          model_dump(exclude_none=True))
+                        ])
+                        self.streamed_args_for_tool[
+                            self.current_tool_id] += argument_diff
+
+            self.prev_tool_call_arr = tool_call_arr
+            return delta
+
+        except Exception as e:
+            logger.error("Error trying to handle streaming tool call: %s", e)
+            logger.debug(
+                "Skipping chunk as a result of tool streaming extraction "
+                "error")
+            return None
+
